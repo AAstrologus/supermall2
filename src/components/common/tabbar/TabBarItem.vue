6e73db8c5f35d52cfd06ecabb1ceba3048079e2b
Bernard <1745534634@qq.com>
2021-01-13 01:32:38 +0800

    增加底部菜单栏和底部导航栏，增加轮播图

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    <div v-if="!isActive">
+      <slot name="item-icon"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-icon-active"></slot>
+    </div>
+    <div :class="{active:isActive}">
+      <slot name="icon-text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TabBarItem",
+  props:{
+    path:String
+  },
+  computed:{
+    isActive() {
+      return this.$route.path.indexOf(this.path)!==-1
+    }
+  },
+  methods:{
+    itemClick() {
+      this.$router.replace(this.path)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .tab-bar-item{
+    flex: 1;
+    text-align: center;
+    height: 49px;
+  }
+  .tab-bar-item img {
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    vertical-align: middle;
+    margin-bottom: 2px;
+  }
+  .active{
+    color:red;
+  }
+</style>
--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,7 @@ module.exports={
         'assets':'@/assets',
         'common':'@/common',
         'components':'@/components',
+        'network':'@/network',
         'views':'@/views'
       }
     }
